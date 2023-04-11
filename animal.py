67dae8d0d79df449310b21a9c70f3e4b22c40c91
Andriéria <andrieria.ad@gmail.com>
2023-04-11 11:27:39 -0300

    introducao poo

---
+++ animal.py
@@ -0,0 +1,29 @@
+class Animal:
+    def __init__(self, nome, peso, posicao):
+        self.__nome = nome
+        self.__peso = peso
+        self.__posicao = posicao
+    
+    @property
+    
+    def nome(self):
+        return self.__nome
+
+    def peso(self):
+        return self.__peso
+
+    @setter.peso()
+    def peso(self, peso):
+        self.__peso = peso
+    
+    @property
+    def posicao(self):
+        return self.__posicao
+    
+    @setter.posicao()
+    def posicao(self, pos):
+        self.__posicao = pos0
+
+
+    def moverse(self):
+        self.__posicao+=1
---
+++ pessoa.py
@@ -0,0 +1,4 @@
+class Pessoa:
+    def __init__(self, nome, matricula):
+        self.nome = nome
+        self.matricula = matricula
